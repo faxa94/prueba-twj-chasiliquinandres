85e259da4afca57de4933e4ef2e30b82076bcad2
faxa94 <andre.chasi@gmail.com>
2017-02-25 11:15:15 -0500

    api album y artista

---
+++ prueba/api/controllers/AlbumController.js
@@ -0,0 +1,11 @@
+/**
+ * AlbumController
+ *
+ * @description :: Server-side logic for managing albums
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
---
+++ prueba/api/controllers/ArtistaController.js
@@ -0,0 +1,11 @@
+/**
+ * ArtistaController
+ *
+ * @description :: Server-side logic for managing artistas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
